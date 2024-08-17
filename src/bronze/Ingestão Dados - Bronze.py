865997713c9430aca2656ae0199338b89c5e2be6
ViniciusOtoni <ViniciusOtoni>
2024-08-17 01:56:18 +0000

    ingestao parametrizada

--- src/bronze/Ingestão Dados - Bronze.py
+++ src/bronze/Ingestão Dados - Bronze.py
@@ -1,10 +1,18 @@
 # Databricks notebook source
+# DBTITLE 1,Setup
+catalog = "hive_metastore"
+schema = "bronze"
+tableName = dbutils.widgets.get("tablename")
+
+# COMMAND ----------
+
+# DBTITLE 1,Gerando Mount
 
 #config
 account_name = "catalogdatabricksvini"
-account_key = ""
+account_key = "" #BetVORhrr5D1RNc3AuagQCXpyh+ygUFeFnovjUNSp5iidUh7NUrR6DmUxce5okmDhhPuqFHx08Ws+ASt66si8A= =
 container_name = "raw"
-mount_name = "/mnt/project/raw" 
+mount_name = f"/mnt/project/raw/{tableName}" 
 
 #url blob
 source_url = f"wasbs://{container_name}@{account_name}.blob.core.windows.net"
@@ -27,17 +35,34 @@ dbutils.fs.mount(
 
 # COMMAND ----------
 
-df = spark.read.format('csv').load("/mnt/project/raw")
-df.display()
-
-# COMMAND ----------
-
 spark.sql("CREATE DATABASE IF NOT EXISTS bronze")
 
 # COMMAND ----------
 
-(df.coalesce(1).
- write.
- format('delta').
- mode("overwrite").
- saveAsTable("bronze.house_price"))
+# DBTITLE 1,Imports
+def table_exists(catalog, database, table):
+    count = (spark.sql(f"SHOW TABLES FROM {catalog}.{database}")
+             .filter("database = '{database}' AND tableName= '{table}' ")
+             .count())
+    return count == 1
+
+# COMMAND ----------
+
+if not (table_exists(catalog, schema, tableName)):
+    print("Tabela não existente, criando...")
+
+    df = spark.read.format('csv').load(f"/mnt/project/raw/{tableName}")
+
+    (df.coalesce(1).
+        write.
+        format('delta').
+        mode("overwrite").
+        saveAsTable(f"{catalog}.{schema}.{tableName}"))
+else:
+    print("Tabela já existente, ignorando full load...")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC SELECT * FROM hive_metastore.bronze.house_price
