2f4e4bd25a4c746feda1e6f6f86a6dbd186cfc3c
ViniciusOtoni <ViniciusOtoni>
2024-08-19 01:07:37 +0000

    criando arquivo de classe

--- src/bronze/ingestao.py
+++ src/bronze/ingestao.py
@@ -1,7 +1,10 @@
 # Databricks notebook source
 import sys
 sys.path.insert(0, "../lib/")
+
 import utils
+import ingestion
+import os
 
 
 # COMMAND ----------
@@ -9,18 +12,17 @@ import utils
 # DBTITLE 1,Setup
 catalog = "hive_metastore"
 schema = "bronze"
-tableName = dbutils.widgets.get("tablename")
+tableName = "house_price" #dbutils.widgets.get("tablename") #"house_price"
 
 # COMMAND ----------
 
 # DBTITLE 1,Gerando Mount
-
-import os
 #config
+
 account_name = "catalogdatabricksvini"
 account_key = os.getenv("BLOB_STORAGE_ACCOUNT_KEY")
 #"BetVORhrr5D1RNc3AuagQCXpyh+ygUFeFnovjUNSp5iidUh7NUrR6DmUxce5okmDhhPuqFHx08Ws+ASt66si8A= =" 
-print("Account Key:", account_key)
+
 container_name = "raw"
 mount_name = f"/mnt/project/raw/{schema}/full_load/{tableName}" 
 
@@ -29,19 +31,9 @@ source_url = f"wasbs://{container_name}@{account_name}.blob.core.windows.net"
 conf_key = f"fs.azure.account.key.{account_name}.blob.core.windows.net"
 
 
+# COMMAND ----------
 
-if any(mount.mountPoint == mount_name for mount in dbutils.fs.mounts()):
-    print(f"O ponto de montagem '{mount_name}' já existe. Desmontando...")
-    dbutils.fs.unmount(mount_name)
-
-#Montar o Blob Storage
-dbutils.fs.mount(
-  source = source_url,
-  mount_point = mount_name,
-  extra_configs = {conf_key: account_key}
-)
-
-
+utils.
 
 # COMMAND ----------
 
@@ -49,44 +41,10 @@ spark.sql("CREATE DATABASE IF NOT EXISTS bronze")
 
 # COMMAND ----------
 
-class Ingestor:
-
-    def __init__(self, spark, catalog, schemaname, tablename, data_format):
-        self.spark = spark,
-        self.catalog = catalog,
-        self.schemaname = schemaname,
-        self.tablename = tablename,
-        self.format = data_format
-        
-
-    def load(self, path):
-        df = (self.spark
-              .read
-              .format(self.format)
-              .schema(self.data_schema)
-              .load(path))
-        
-        return df
-
-        
-    def save(self, df):
-        (df.coalesce(1)
-            .write
-            .format('delta')
-            .mode("overwrite")
-            .saveAsTable(f"{self.catalog}.{self.schemaname}.{self.tablename}"))
-    
-    def execute(self, path):
-        df = self.load(path)
-        self.save(df)
-
-
-# COMMAND ----------
-
 if not utils.table_exists(spark, catalog, schema, tableName):
     print("Tabela não existe, criando...")
 
-    ingest_full_load = Ingestor(spark = spark,
+    ingest_full_load = ingestion.Ingestor(spark = spark,
                                 catalog=catalog,
                                  schemaname=schema,
                                  tablename=tableName, 
@@ -102,5 +60,7 @@ else:
 # COMMAND ----------
 
 # MAGIC %sql
+# MAGIC
+# MAGIC --visualização
 # MAGIC
 # MAGIC SELECT * FROM hive_metastore.bronze.house_price
--- src/lib/utils.py
+++ src/lib/utils.py
@@ -1,4 +1,5 @@
 import json
+
 from pyspark.sql import types
 
 
@@ -8,3 +9,25 @@ def table_exists(spark, catalog, database, table):
                 .filter(f"database = '{database}' AND tableName = '{table}'")
                 .count())
     return count == 1
+
+
+def create_mount(spark, mount_name, source_url, conf_key, account_key):
+    
+    from pyspark.dbutils import DBUtils
+    dbutils = DBUtils(spark)
+    
+    # Verifica se o ponto de montagem já existe
+    if any(mount.mountPoint == mount_name for mount in dbutils.fs.mounts()):
+        print(f"O ponto de montagem '{mount_name}' já existe.")
+    else:
+        # Montar o Blob Storage
+        dbutils.fs.mount(
+            source=source_url,
+            mount_point=mount_name,
+            extra_configs={conf_key: account_key}
+        )
+
+
+
+
+        
